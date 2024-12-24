eb520cddcfaa6906ee112af1617880b30478711c
isikepalaku <isikepalakreatif@gmail.com>
2024-12-25 00:31:12 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,8 @@ export default defineConfig(({ mode }) => {
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src')
-      }
+      },
+      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
     },
     define: {
       'process.env': env

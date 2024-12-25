314e1294f3cb66a2cf2ca89d6e337e45e9872899
isikepalaku <isikepalakreatif@gmail.com>
2024-12-25 16:07:57 +0800

    fix

--- server/index.ts
+++ server/index.ts
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5086;
 
 // Add logging middleware
 app.use((req, res, next) => {
